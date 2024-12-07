a5d2e37635b8e1871ba806c3c24f869eff1729a3
Dhanush51 <158693712+Dhanush51@users.noreply.github.com>
2024-12-07 14:42:54 +0530

    Update api.js

--- api.js
+++ api.js
@@ -10,7 +10,7 @@ import UserModel from "./model/User.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors)
+app.use(cors())
 //     ({
 //     origin: "https://loginapp-7ddh.onrender.com",
 //     credentials: true
